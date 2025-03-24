fc0ef0e4a3a7fe18704638c0afc831ae58574bc6
parmarkrishnaa <parmarkrishna050611@gmail.com>
2025-03-24 22:24:08 +0530

    first commit

---
+++ Day-1/demo.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Krishna" << endl;
+    cout << "Parmar" << endl;
+    cout << "I live in Navsari." << endl;
+    cout << "I'm form Gujarat" << endl;
+    cout << "I'm 19 years old." << endl;
+
+    return 0;
+}
---
+++ Day-1/demo2.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    string name;
+    int age, number;
+
+    cout << "Enter your name: ";
+    cin >> name;
+    
+    cout << "Enter your age: ";
+    cin >> age;
+
+    cout << "Enter your number: ";
+    cin >> number;
+
+    cout << "Your name is " << name << endl;
+    cout << "Your age is " << age << endl;
+    cout << "Your number is " << number << endl;
+
+    return 0;
+}
